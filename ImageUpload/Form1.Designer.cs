d5bfd8e63fccd1780cb20709194366f842726ba6
wizzed <wizmao@gmail.com>
2016-05-22 18:07:01 +0800

    First Commit

---
+++ ImageUpload/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ImageUpload
+{
+    partial class ImageUpload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageUpload));
+            this.checkTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkTimer
+            // 
+            this.checkTimer.Enabled = true;
+            this.checkTimer.Interval = 200;
+            this.checkTimer.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-2, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Success 00:00:00";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ImageUpload_MouseDoubleClick);
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            // 
+            // ImageUpload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(81, 16);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ImageUpload";
+            this.Opacity = 0.7D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Upload!";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ImageUpload_MouseDoubleClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer checkTimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ImageUpload/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+namespace ImageUpload
+{
+    public partial class ImageUpload : Form
+    {
+        public ImageUpload()
+        {
+            InitializeComponent();
+        }
+
+        private static ImageCodecInfo GetEncoderInfo(String mimeType)
+        {
+            int j;
+            ImageCodecInfo[] encoders;
+            encoders = ImageCodecInfo.GetImageEncoders();
+            for (j = 0; j < encoders.Length; ++j)
+            {
+                if (encoders[j].MimeType == mimeType)
+                    return encoders[j];
+            }
+            return null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(95, 18);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((char)e.KeyChar == (char)Keys.Escape)
+                Application.Exit();
+        }
+
+        /*private void timer1_Tick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }*/
+
+        //FOR UPLOADING TO SERVER
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            uploadToSite();
+        }
+
+        string path;
+
+        bool uploading = false;
+        private void uploadToSite()
+        {
+            if (Clipboard.ContainsImage() == true && !uploading)
+            {
+                label1.Text = "Uploading...";
+                uploading = true;
+                string website;
+                path = Directory.GetCurrentDirectory() + "\\" + DateTime.Now.ToString("yyyyMMdd HHmmss") + ".jpg";
+                website = "http://wizzed.net/imageupload/uploadz.php";
+                System.Net.WebClient client = new System.Net.WebClient();
+                client.Headers.Add("Content-Type", "binary/octet-stream");
+                
+                Image test = Clipboard.GetImage();
+                EncoderParameters parameters = new EncoderParameters(1);
+                parameters.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 80L);
+                ImageCodecInfo codecInfo = GetEncoderInfo("image/jpeg");
+                test.Save(path, codecInfo, parameters);
+                Object o = new Object();
+                Uri webUri = new Uri(website);
+                client.UploadFileCompleted += new UploadFileCompletedEventHandler(UploadFileCallback);
+                client.UploadFileAsync(webUri, "POST", path, o);
+                //timer1.Start();
+            }
+        }
+
+        private void UploadFileCallback(Object sender, UploadFileCompletedEventArgs e)
+        {
+            byte[] result = e.Result;
+            string s = System.Text.Encoding.UTF8.GetString(result, 0, result.Length);
+            label1.Text = s.Split('$')[0] + " " + DateTime.Now.ToString("HH:mm:ss");
+            System.IO.File.Delete(path);
+            Clipboard.SetText(s.Split('$')[1]);
+            uploading = false;
+        }
+
+       
+        private void saveToFolder()
+        {
+            if (Clipboard.ContainsImage() == true)
+            {
+                string path =  Directory.GetCurrentDirectory() + "\\" + DateTime.Now.ToString("yyyyMMdd HHmmss") + ".png";
+                
+                //Image test = Clipboard.GetImage();
+
+                try
+                {
+                    Image test = Clipboard.GetImage();
+                    if (test != null)
+                    {
+                        test = Clipboard.GetImage();
+                        Clipboard.Clear();
+                        EncoderParameters parameters = new EncoderParameters(1);
+                        parameters.Param[0] = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 50L);
+                        ImageCodecInfo codecInfo = GetEncoderInfo("image/png");
+                        test.Save(path, codecInfo, parameters);
+                        label1.Text = DateTime.Now.ToString("yyyyMMdd HHmmss") + ".png";
+                        //timer1.Start();
+                    }
+                }
+                catch(Exception e)
+                {
+                }
+               
+            }
+        }
+
+        #region drag to move window
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+
+        }
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+        #endregion
+
+        /*
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            timer3.Stop();
+        }*/
+
+        private void ImageUpload_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://i.wizzed.net/images.php");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
